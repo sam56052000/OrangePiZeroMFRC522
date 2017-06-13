800ed231e49d855df53dd473a873876382b64bda
Izaac <izaac.kirscht@gmail.com>
2017-06-13 15:43:36 -0500

    Create getAllTheStuff.sh

---
+++ getAllTheStuff.sh
@@ -0,0 +1,10 @@
+apt-get install python-dev
+apt-get install python-pip
+
+git clone https://github.com/rm-hull/OPi.GPIO.git
+cd OPi.GPIO
+pip install .
+
+cd ..
+git clone https://github.com/BiTinerary/OrangePiZeroMFRC522.git
+python ./OrangePiZeroMFRC522/MFRC522-python/Read.py
