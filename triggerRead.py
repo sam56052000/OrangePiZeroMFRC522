7472e12b4da6ddad5108e1d8378e5c34a69f74b5
BiTinerary <izaac.kirscht@gmail.com>
2019-05-04 14:33:21 -0500

    meh

--- triggerRead.py
+++ triggerRead.py
@@ -9,10 +9,10 @@ continue_reading = True
 
 # Capture SIGINT for cleanup when the script is aborted
 def end_read(signal,frame):
-    global continue_reading
-    print "\nCtrl+C captured, ending read."
-    continue_reading = False
-    GPIO.cleanup()
+	global continue_reading
+	print "\nCtrl+C captured, ending read."
+	continue_reading = False
+	GPIO.cleanup()
 
 # Hook the SIGINT
 signal.signal(signal.SIGINT, end_read)
@@ -25,29 +25,28 @@ print "\nWelcome to the MFRC522 data read example"
 
 # This loop keeps checking for chips. If one is near it will get the UID and authenticate
 while continue_reading:
-    
-    # Scan for cards    
-    (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+	
+	# Scan for cards	
+	(status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
 
-    # If a card is found
-    if status == MIFAREReader.MI_OK:
-        print "Card detected"
-    
-    # Get the UID of the card
-    (status,uid) = MIFAREReader.MFRC522_Anticoll()
+	# If a card is found
+	if status == MIFAREReader.MI_OK:
+		print "Card detected"
+	
+	# Get the UID of the card
+	(status,uid) = MIFAREReader.MFRC522_Anticoll()
 
-    # If we have the UID, continue
-    if status == MIFAREReader.MI_OK:
+	# If we have the UID, continue
+	if status == MIFAREReader.MI_OK:
 
-        # Print UID
+		# Print UID
 	uidFull = "%s-%s-%s-%s" % (uid[0], uid[1], uid[2], uid[3])
-        #print "Card read UID: "+str(uid[0])+","+str(uid[1])+","+str(uid[2])+","+str(uid[3])
+		#print "Card read UID: "+str(uid[0])+","+str(uid[1])+","+str(uid[2])+","+str(uid[3])
 	
 	hashie = {}
 	with open('hashFile.txt') as file:
 		for line in file:
 			key, value = line.split(',')
-			print key, value,
 			hashie[key] = value
 	
 	if uidFull in hashie:
@@ -55,23 +54,25 @@ while continue_reading:
 		os.system('%s' % hashie[uidFull])
 	else:
 		print "UID: %s is not associated with a trigger" % uidFull
-	
-	os.system('echo "heartbeat" > /sys/class/leds/red_led/trigger')
-	
-        # This is the default key for authentication
-        key = [0xFF,0xFF,0xFF,0xFF,0xFF,0xFF]
-        
-        # Select the scanned tag
-        MIFAREReader.MFRC522_SelectTag(uid)
+		for x in range(3):
 
-        # Authenticate
-        status = MIFAREReader.MFRC522_Auth(MIFAREReader.PICC_AUTHENT1A, 8, key, uid)
+			time.sleep(.2)
+			os.system('echo 1 > /sys/class/leds/red_led/brightness')
+			time.sleep(.2)
 
-        # Check if authenticated
-        if status == MIFAREReader.MI_OK:
-            MIFAREReader.MFRC522_Read(8)
-            MIFAREReader.MFRC522_StopCrypto1()
-	    time.sleep(2)
-	    os.system('echo 0 > /sys/class/leds/red_led/brightness')
+		# This is the default key for authentication
+		key = [0xFF,0xFF,0xFF,0xFF,0xFF,0xFF]
+		
+		# Select the scanned tag
+		MIFAREReader.MFRC522_SelectTag(uid)
+
+		# Authenticate
+		status = MIFAREReader.MFRC522_Auth(MIFAREReader.PICC_AUTHENT1A, 8, key, uid)
+
+		# Check if authenticated
+		if status == MIFAREReader.MI_OK:
+			MIFAREReader.MFRC522_Read(8)
+			MIFAREReader.MFRC522_StopCrypto1()
+		time.sleep(2)
 	else:
-            print "Authentication error"
+			print "Authentication error"
--- triggerRead2.py
+++ triggerRead2.py
@@ -2,15 +2,23 @@
 # -*- coding: utf8 -*-
 
 import OPi.GPIO as GPIO
-import MFRC522
-import signal
+from MFRC522 import MFRC522
+import signal, time, os
+from punchCard import clock
 
 continue_reading = True
+action = True
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(15, GPIO.OUT)
+GPIO.output(15, GPIO.HIGH)
+#GPIO.setup(7, GPIO.OUT)
+#GPIO.output(7, GPIO.LOW)
 
 # Capture SIGINT for cleanup when the script is aborted
 def end_read(signal,frame):
     global continue_reading
-    print "Ctrl+C captured, ending read."
+    print "\nCtrl+C captured, ending read."
     continue_reading = False
     GPIO.cleanup()
 
@@ -21,19 +29,18 @@ signal.signal(signal.SIGINT, end_read)
 MIFAREReader = MFRC522.MFRC522()
 
 # Welcome message
-print "Welcome to the MFRC522 data read example"
-print "Press Ctrl-C to stop."
+print "\nWelcome to the MFRC522 data read example"
 
 # This loop keeps checking for chips. If one is near it will get the UID and authenticate
 while continue_reading:
-    
-    # Scan for cards    
+
+    # Scan for cards
     (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
 
     # If a card is found
     if status == MIFAREReader.MI_OK:
         print "Card detected"
-    
+
     # Get the UID of the card
     (status,uid) = MIFAREReader.MFRC522_Anticoll()
 
@@ -41,11 +48,31 @@ while continue_reading:
     if status == MIFAREReader.MI_OK:
 
         # Print UID
-        print "Card read UID: "+str(uid[0])+","+str(uid[1])+","+str(uid[2])+","+str(uid[3])
-    
+        uidFull = "%s-%s-%s-%s" % (uid[0], uid[1], uid[2], uid[3])
+        #print "Card read UID: "+str(uid[0])+","+str(uid[1])+","+str(uid[2])+","+str(uid[3])
+
+        hashie = {}
+        with open('hashFile.txt') as file:
+                for line in file:
+                        key, value = line.split(',')
+                        hashie[key] = value
+
+        if uidFull in hashie:
+                print uidFull
+                clock(action)
+                action = not action
+        else:
+                print "UID: %s is not associated with a trigger" % uidFull
+
+                for x in range(3):
+                        time.sleep(.2)
+                        os.system('echo 1 > /sys/class/leds/red_led/brightness')
+                        time.sleep(.2)
+                        os.system('echo 0 > /sys/class/leds/red_led/brightness')
+                        time.sleep(.2)
         # This is the default key for authentication
         key = [0xFF,0xFF,0xFF,0xFF,0xFF,0xFF]
-        
+
         # Select the scanned tag
         MIFAREReader.MFRC522_SelectTag(uid)
 
@@ -56,6 +83,7 @@ while continue_reading:
         if status == MIFAREReader.MI_OK:
             MIFAREReader.MFRC522_Read(8)
             MIFAREReader.MFRC522_StopCrypto1()
+            time.sleep(2)
         else:
-            print "Authentication error"
-
+            ""
+        time.sleep(1)
